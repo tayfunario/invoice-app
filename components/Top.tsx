3cf9e5ff5cb87fc51f58d9b8340e45a0ead3ca0e
tayfunetta <tayfunemrekahraman@gmail.com>
2023-11-19 18:33:26 +0300

    Add Top component and refactor code structure

---
+++ components/Top.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+const ulVariants = {
+  open: {
+    opacity: 1,
+    scale: 1,
+    borderRadius: "0%",
+    transition: {
+      staggerChildren: 0.07,
+      delayChildren: 0.2,
+    },
+  },
+  closed: {
+    opacity: 0,
+    scale: 0,
+    borderRadius: "50%",
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+function Top() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      const target = e.target as HTMLElement;
+      const ulButtons = document.querySelectorAll(".ul-btn");
+      const toggleControl = document.getElementById("toggle-control");
+
+      if (target !== toggleControl && !target.classList.contains("ul-btn")) {
+        setOpen(false);
+      }
+    });
+  }, []);
+
+  return (
+    <section className="flex justify-between mx-auto mt-10 px-6 text-black">
+      <div>
+        <h1 className="text-2xl font-bold leading-5 tracking-tighter">
+          Invoices
+        </h1>
+        <span className="text-sm text-gray-500">7 invoices</span>
+      </div>
+
+      <div className="relative flex gap-x-4">
+        <div className="flex items-center px-2 font-bold rounded-2xl">
+          <button
+            id="toggle-control"
+            className="flex items-center gap-x-2"
+            onClick={() => setOpen(!open)}
+          >
+            Filter <img src="icon-arrow-down.svg" />
+          </button>
+          <motion.ul
+            className="absolute top-10 origin-top right-20 w-24 p-1 text-md font-normal bg-white shadow-md"
+            variants={ulVariants}
+            animate={open ? "open" : "closed"}
+          >
+            <li>
+              <button className="ul-btn w-full px-1 text-start rounded-md bg-yellow-100 hover:bg-yellow-200 text-yellow-700">
+                Pending
+              </button>
+            </li>
+            <li>
+              <button className="ul-btn w-full px-1 my-1 text-start rounded-md bg-green-100 hover:bg-green-200 text-green-700">
+                Paid
+              </button>
+            </li>
+            <li>
+              <button className="ul-btn w-full px-1 text-start rounded-md bg-gray-100 hover:bg-gray-200 text-gray-700">
+                Draft
+              </button>
+            </li>
+          </motion.ul>
+        </div>
+        <button className="flex items-center gap-x-2 my-[2px] pl-1 pr-3 text-white text-sm font-bold bg-customPurple rounded-3xl">
+          <img
+            src="icon-plus.svg"
+            className="p-[9px] m-0 bg-white rounded-full"
+          />{" "}
+          <span className="mt-px">New</span>
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Top;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,12 @@
 import Header from "../components/Header";
+import Top from "../components/Top";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-lightBG">
       <Header />
       <main>
-        <div className="flex justify-between mx-auto mt-10 px-6 text-black">
-          <div>
-            <h1 className="text-2xl font-bold leading-5 tracking-tighter">
-              Invoices
-            </h1>
-            <span className="text-sm text-gray-500">7 invoices</span>
-          </div>
-
-          <div className="flex gap-x-4">
-            <div className="flex items-center font-bold gap-x-2">
-              Filter <img src="icon-arrow-down.svg" />
-            </div>
-            <button className="flex items-center gap-x-2 py-[5px] pl-1 pr-3 text-white text-sm font-bold bg-customPurple rounded-3xl">
-              <img
-                src="icon-plus.svg"
-                className="p-[10px] bg-white rounded-full"
-              />{" "}
-              <span className="mt-px">New</span>
-            </button>
-          </div>
-        </div>
+        <Top />
       </main>
     </div>
   );
