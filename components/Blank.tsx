60c1d0a66a2b199200aed0c9991391829713e4f0
tayfunetta <tayfunemrekahraman@gmail.com>
2023-12-02 15:02:04 +0300

    added dark styles to Blank

--- components/Blank.tsx
+++ components/Blank.tsx
@@ -6,9 +6,9 @@ function Blank() {
         className="w-36 mx-auto"
         alt="No invoices"
       />
-      <figcaption className="text-xl text-center text-grayishViolet font-bold mt-8">
+      <figcaption className="text-xl text-center dark:text-white font-bold mt-8">
         There is nothing here
-        <p className="w-40 text-center text-darkerGray mx-auto mt-4 text-xs font-normal">
+        <p className="w-40 text-center text-darkerGray dark:text-lightGray mx-auto mt-4 text-xs font-normal">
           Create an invoice by clicking the New button and get started
         </p>
       </figcaption>
